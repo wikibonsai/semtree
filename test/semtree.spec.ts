842759671951ff299d2ef5a386f0b1a0059a6251
manunamz <manunamz@pm.me>
2024-07-24 16:54:18 -0400

    ref: cleanup test structure.

--- test/semtree.spec.ts
+++ test/semtree.spec.ts
@@ -27,83 +27,91 @@ import { outputWithID } from './fixtures/output';
 import { SemTree } from '../src/index';
 
 
-let fakeConsoleWarn: any;
+let fakeConsoleWarn: sinon.SinonSpy;
 let semtree: SemTree;
-let fakeConsoleLog: any;
+let fakeConsoleLog: sinon.SinonSpy;
 
 describe('semtree', () => {
 
-  describe('virtual trunk', () => {
+  beforeEach(() => {
+    console.warn = (msg) => msg + '\n';
+    fakeConsoleWarn = sinon.spy(console, 'warn');
+    console.log = (msg) => msg + '\n';
+    fakeConsoleLog = sinon.spy(console, 'log');
+  });
 
-    beforeEach(() => {
-      console.warn = (msg) =>  msg + '\n';
-      fakeConsoleWarn = sinon.spy(console, 'warn');
-      semtree = new SemTree({
-        testing: true,
-        virtualTrunk: true,
+  afterEach(() => {
+    fakeConsoleWarn.restore();
+    fakeConsoleLog.restore();
+  });
+
+  describe('parse()', () => {
+
+    describe('with virtual trunk', () => {
+
+      beforeEach(() => {
+        semtree = new SemTree({
+          testing: true,
+          virtualTrunk: true,
+        });
       });
-      // suppress console
-      console.log = (msg) => msg + '\n';
-      // fake console
-      fakeConsoleLog = sinon.spy(console, 'log');
-    });
 
-    afterEach(() => {
-      fakeConsoleLog.restore();
-    });
+      it('default; leave existing id; 2 spaces; wiki', () => {
+        assert.deepEqual(semtree.parse(wikiContentWithIDWithSpaces), treeWithIDVirtualTrunk);
+      });
 
-    describe('parse', () => {
+      it('no mkdn list; no suffix', () => {
+        semtree.opts({
+          mkdnList: false,
+          suffix: 'none',
+        });
+        assert.deepEqual(semtree.parse(content), treeNoSuffix);
+      });
+
+      it('append location in tree', () => {
+        semtree.opts({
+          suffix: 'loc',
+          wikitext: false,
+        });
+        assert.deepEqual(semtree.parse(content), treeWithLoc);
+      });
+
+      it('append id', () => {
+        semtree.opts({
+          suffix: 'id',
+          wikitext: false,
+        });
+        assert.deepEqual(semtree.parse(content), treeWithIDVirtualTrunk);
+      });
+
+      it('leave existing id; 2 spaces', () => {
+        semtree.opts({ wikitext: false });
+        assert.deepEqual(semtree.parse(contentWithIDWithSpaces), treeWithIDVirtualTrunk);
+      });
+
+      it('leave existing id; 3 spaces', () => {
+        semtree.opts({ wikitext: false });
+        assert.deepEqual(semtree.parse(contentWithIDWith3Spaces), treeWithIDVirtualTrunk);
+      });
+
+      it('leave existing id; tabs', () => {
+        semtree.opts({ wikitext: false });
+        assert.deepEqual(semtree.parse(contentWithIDWithTabs), treeWithIDVirtualTrunk);
+      });
+
+      it('error; duplicate text (leave existing id; 2 spaces; wiki)', () => {
+        assert.strictEqual(
+          semtree.parse(wikiContentWithDuplicatesWithSpaces),
+          'tree did not build, duplicate nodes found:\n\nroot-(0a1b2)\n\n'
+        );
+      });
 
       describe('single file', () => {
 
         it('default; leave existing id; 2 spaces; wiki', () => {
-          assert.deepEqual(semtree.parse(wikiContentWithIDWithSpaces), treeWithIDVirtualTrunk);
-        });
-
-        it('no mkdn list; no suffix', () => {
-          semtree.opts({
-            mkdnList: false,
-            suffix: 'none',
-          });
-          assert.deepEqual(semtree.parse(content), treeNoSuffix);
-        });
-
-        it('append location in tree', () => {
-          semtree.opts({
-            suffix: 'loc',
-            wikitext: false,
-          });
-          assert.deepEqual(semtree.parse(content), treeWithLoc);
-        });
-
-        it('append id', () => {
-          semtree.opts({
-            suffix: 'id',
-            wikitext: false,
-          });
-          assert.deepEqual(semtree.parse(content), treeWithIDVirtualTrunk);
-        });
-
-        it('leave existing id; 2 spaces', () => {
-          semtree.opts({ wikitext: false });
-          assert.deepEqual(semtree.parse(contentWithIDWithSpaces), treeWithIDVirtualTrunk);
-        });
-
-        it('leave existing id; 3 spaces', () => {
-          semtree.opts({ wikitext: false });
-          assert.deepEqual(semtree.parse(contentWithIDWith3Spaces), treeWithIDVirtualTrunk);
-        });
-
-        it('leave existing id; tabs', () => {
-          semtree.opts({ wikitext: false });
-          assert.deepEqual(semtree.parse(contentWithIDWithTabs), treeWithIDVirtualTrunk);
-        });
-
-        it('error; duplicate text (leave existing id; 2 spaces; wiki)', () => {
-          assert.strictEqual(
-            semtree.parse(wikiContentWithDuplicatesWithSpaces),
-            'tree did not build, duplicate nodes found:\n\nroot-(0a1b2)\n\n'
-          );
+          semtree.parse(wikiContentWithIDWithSpaces);
+          semtree.print();
+          assert.deepEqual(fakeConsoleLog.getCall(0).args[0], outputWithID);
         });
 
       });
@@ -128,31 +136,15 @@ describe('semtree', () => {
 
       });
 
-      describe('single file', () => {
+    });
 
-        it('default; leave existing id; 2 spaces; wiki', () => {
-          semtree.parse(wikiContentWithIDWithSpaces);
-          semtree.print();
-          assert.deepEqual(fakeConsoleLog.getCall(0).args[0], outputWithID);
+    describe('with concrete trunk', () => {
+
+      beforeEach(() => {
+        semtree = new SemTree({
+          testing: true
         });
-
       });
-
-    });
-
-  });
-
-  describe('concrete trunk', () => {
-
-    beforeEach(() => {
-      console.warn = (msg) => msg + '\n';
-      fakeConsoleWarn = sinon.spy(console, 'warn');
-      semtree = new SemTree({
-        testing: true
-      });
-    });
-
-    describe('parse; multi file; concrete trunk', () => {
 
       it('default; leave existing id; 2 spaces; wiki', () => {
         assert.deepEqual(semtree.parse(wikiContentsWithIDWithSpaces, 'root'), treeWithIDConcreteTrunk);
@@ -174,106 +166,95 @@ describe('semtree', () => {
 
     });
 
-    describe('updateSubTree', () => {
+  });
 
-      // this test describes the behavior related to adding a link to a generic doc
-      it('add leaf', () => {
-        // setup
-        semtree.parse(wikiContentsWithIDWithSpaces, 'root');
-        // go
-        const wikiGraphContentWithIDWithSpaces = 
+  describe('updateSubTree()', () => {
+
+    beforeEach(() => {
+      semtree = new SemTree({
+        testing: true
+      });
+    });
+
+    it('add leaf', () => {
+      semtree.parse(wikiContentsWithIDWithSpaces, 'root');
+      const wikiGraphContentWithIDWithSpaces = 
 `- [[graph-(0a1b2)]]
   - [[tree-(0a1b2)]]
   - [[web-(0a1b2)]]
   - [[newChild-(0a1b2)]]
 `;
-        semtree.updateSubTree({ 'graph': wikiGraphContentWithIDWithSpaces }, 'graph');
-        // prep expected data
-        const expectedUpdatedTree = JSON.parse(JSON.stringify(treeWithIDConcreteTrunk));
-        const nodeIndex = expectedUpdatedTree.findIndex((node: TreeNode) => node.text === 'graph-(0a1b2)');
-        expectedUpdatedTree[nodeIndex].children.push('newChild-(0a1b2)');
-        expectedUpdatedTree.push(
-          {
-            'text': 'newChild-(0a1b2)',
-            'ancestors': ['root', 'root-(0a1b2)', 'graph', 'graph-(0a1b2)'],
-            'children': [],
-          },
-        );
-        // assert
-        for (const expdNode of expectedUpdatedTree) {
-          assert.deepStrictEqual(semtree.nodes.find((n: TreeNode) => n.text === expdNode.text), expdNode);
-        }
-      });
+      semtree.updateSubTree({ 'graph': wikiGraphContentWithIDWithSpaces }, 'graph');
+      const expectedUpdatedTree = JSON.parse(JSON.stringify(treeWithIDConcreteTrunk));
+      const nodeIndex = expectedUpdatedTree.findIndex((node: TreeNode) => node.text === 'graph-(0a1b2)');
+      expectedUpdatedTree[nodeIndex].children.push('newChild-(0a1b2)');
+      expectedUpdatedTree.push(
+        {
+          'text': 'newChild-(0a1b2)',
+          'ancestors': ['root', 'root-(0a1b2)', 'graph', 'graph-(0a1b2)'],
+          'children': [],
+        },
+      );
+      for (const expdNode of expectedUpdatedTree) {
+        assert.deepStrictEqual(semtree.nodes.find((n: TreeNode) => n.text === expdNode.text), expdNode);
+      }
+    });
 
-      // this test describes the behavior related to removing a link to a generic doc
-      it('remove leaf', () => {
-        // setup
-        semtree.parse(wikiContentsWithIDWithSpaces, 'root');
-        // go
-        const wikiGraphContentWithIDWithSpaces = 
+    it('remove leaf', () => {
+      semtree.parse(wikiContentsWithIDWithSpaces, 'root');
+      const wikiGraphContentWithIDWithSpaces = 
 `- [[graph-(0a1b2)]]
   - [[web-(0a1b2)]]
 `;
-        semtree.updateSubTree({ 'graph': wikiGraphContentWithIDWithSpaces }, 'graph');
-        // prep expected data
-        const expectedUpdatedTree = JSON.parse(JSON.stringify(treeWithIDConcreteTrunk));
-        const parentNodeIndex = expectedUpdatedTree.findIndex((node: TreeNode) => node.text === 'graph-(0a1b2)');
-        expectedUpdatedTree[parentNodeIndex].children = ['web-(0a1b2)'];
-        const nodeIndex = expectedUpdatedTree.findIndex((node: TreeNode) => node.text === 'tree-(0a1b2)');
-        expectedUpdatedTree.splice(nodeIndex, 1);
-        // assert
-        for (const expdNode of expectedUpdatedTree) {
-          assert.deepStrictEqual(semtree.nodes.find((n: TreeNode) => n.text === expdNode.text), expdNode);
-        }
-      });
+      semtree.updateSubTree({ 'graph': wikiGraphContentWithIDWithSpaces }, 'graph');
+      const expectedUpdatedTree = JSON.parse(JSON.stringify(treeWithIDConcreteTrunk));
+      const parentNodeIndex = expectedUpdatedTree.findIndex((node: TreeNode) => node.text === 'graph-(0a1b2)');
+      expectedUpdatedTree[parentNodeIndex].children = ['web-(0a1b2)'];
+      const nodeIndex = expectedUpdatedTree.findIndex((node: TreeNode) => node.text === 'tree-(0a1b2)');
+      expectedUpdatedTree.splice(nodeIndex, 1);
+      for (const expdNode of expectedUpdatedTree) {
+        assert.deepStrictEqual(semtree.nodes.find((n: TreeNode) => n.text === expdNode.text), expdNode);
+      }
+    });
 
-      // this test describes the behavior related to adding a link to an index doc (with content)
-      it('add trunk', () => {
-        // setup
-        semtree.parse(wikiContentsWithIDWithSpaces, 'root');
-        // go
-        const wikiGraphContentWithIDWithSpaces = 
+    it('add trunk', () => {
+      semtree.parse(wikiContentsWithIDWithSpaces, 'root');
+      const wikiGraphContentWithIDWithSpaces = 
 `- [[graph-(0a1b2)]]
   - [[tree-(0a1b2)]]
   - [[web-(0a1b2)]]
   - [[newbranch]]
 `;
-        const newBranchContent =
+      const newBranchContent =
 `- [[newbranchcontent]]
 `;
-        semtree.updateSubTree({
-          'graph': wikiGraphContentWithIDWithSpaces,
-          'newbranch': newBranchContent,
-        }, 'graph');
-        // prep expected data
-        const expectedUpdatedTree = JSON.parse(JSON.stringify(treeWithIDConcreteTrunk));
-        const nodeIndex = expectedUpdatedTree.findIndex((node: TreeNode) => node.text === 'graph-(0a1b2)');
-        expectedUpdatedTree[nodeIndex].children.push('newbranch');
-        expectedUpdatedTree.push(
-          {
-            'text': 'newbranch',
-            'ancestors': ['root', 'root-(0a1b2)', 'graph', 'graph-(0a1b2)'],
-            'children': ['newbranchcontent'], },
-          { 
-            'text': 'newbranchcontent',
-            'ancestors': ['root', 'root-(0a1b2)', 'graph', 'graph-(0a1b2)', 'newbranch'],
-            'children': [],
-          },
-        );
-        // assert
-        for (const expdNode of expectedUpdatedTree) {
-          assert.deepStrictEqual(semtree.nodes.find((n: TreeNode) => n.text === expdNode.text), expdNode);
-        }
-      });
+      semtree.updateSubTree({
+        'graph': wikiGraphContentWithIDWithSpaces,
+        'newbranch': newBranchContent,
+      }, 'graph');
+      const expectedUpdatedTree = JSON.parse(JSON.stringify(treeWithIDConcreteTrunk));
+      const nodeIndex = expectedUpdatedTree.findIndex((node: TreeNode) => node.text === 'graph-(0a1b2)');
+      expectedUpdatedTree[nodeIndex].children.push('newbranch');
+      expectedUpdatedTree.push(
+        {
+          'text': 'newbranch',
+          'ancestors': ['root', 'root-(0a1b2)', 'graph', 'graph-(0a1b2)'],
+          'children': ['newbranchcontent'], },
+        { 
+          'text': 'newbranchcontent',
+          'ancestors': ['root', 'root-(0a1b2)', 'graph', 'graph-(0a1b2)', 'newbranch'],
+          'children': [],
+        },
+      );
+      for (const expdNode of expectedUpdatedTree) {
+        assert.deepStrictEqual(semtree.nodes.find((n: TreeNode) => n.text === expdNode.text), expdNode);
+      }
+    });
 
-      // uses a different data structure than other 'updateSubTree' tests.
+    describe('remove trunk', () => {
 
-      // this test describes the behavior related to removing a link to an index doc, which does not contain links to other index docs
-      it('remove trunk; clean', () => {
-        // setup
+      it('clean', () => {
         semtree.parse(wikiContentsIndexAndEntrySiblings, 'i.bonsai');
-        // go
-        // this removes 'i.education'
         const rmBranch = 
 `- [[social-science]]
   - [[discourse]]
@@ -281,7 +262,6 @@ describe('semtree', () => {
         semtree.updateSubTree({
           'i.social-science': rmBranch
         }, 'i.social-science');
-        // prep expected data...
         const tree: TreeNode[] = JSON.parse(JSON.stringify(treeIndexAndEntrySiblings));
         const expectedUpdatedTree: TreeNode[] = tree.filter((node: TreeNode) => ((node.text !== 'i.education')
                                                                                 && (node.text !== 'learning-theory')
@@ -293,23 +273,16 @@ describe('semtree', () => {
                                                                               || (node.text === 'classical-conditioning')));
         const node: TreeNode | undefined = expectedUpdatedTree.find((node: TreeNode) => node.text === 'social-science');
         if (node) node.children = ['discourse'];
-        // assert
-        // existing nodes
         for (const expdNode of expectedUpdatedTree) {
           assert.deepStrictEqual(semtree.nodes.find((n: TreeNode) => n.text === expdNode.text), expdNode);
         }
-        // removed nodes
         for (const rmNode of expectedRmedNodes) {
           assert.strictEqual(semtree.nodes.find((n: TreeNode) => n.text === rmNode.text), undefined);
         }
       });
 
-      // this test describes the behavior related to removing a link to an index doc, which contains links to other index docs
-      it('remove trunk; with remaining dead branch', () => {
-        // setup
+      it('with remaining dead branch', () => {
         semtree.parse(wikiContentsIndexAndEntrySiblings, 'i.bonsai');
-        // go
-        // this removes 'i.social-science' and by extension, 'i.education'
         const rmBranch = 
 `- [[root]]
   - [[semantic-tree]]
@@ -317,28 +290,23 @@ describe('semtree', () => {
         semtree.updateSubTree({
           'i.bonsai': rmBranch,
         }, 'i.bonsai');
-        // prep expected data...
         let expectedUpdatedTree = JSON.parse(JSON.stringify(treeIndexAndEntrySiblings));
         expectedUpdatedTree = expectedUpdatedTree.filter((node: TreeNode) => ((node.text === 'i.bonsai') || (node.text === 'root') || (node.text === 'semantic-tree')));
         expectedUpdatedTree.find((node: TreeNode) => node.text === 'semantic-tree').children = [];
-        // assert
         for (const expdNode of expectedUpdatedTree) {
           assert.deepStrictEqual(semtree.nodes.find((n: TreeNode) => n.text === expdNode.text), expdNode);
         }
-        // branches
         assert.strictEqual(semtree.nodes.find((n: TreeNode) => n.text === 'i.social-science'), undefined);
         assert.strictEqual(semtree.nodes.find((n: TreeNode) => n.text === 'i.education'), undefined);
-        // leaves
         assert.strictEqual(semtree.nodes.find((n: TreeNode) => n.text === 'social-science'), undefined);
         assert.strictEqual(semtree.nodes.find((n: TreeNode) => n.text === 'education'), undefined);
       });
+    });
 
-      describe.skip('fail', () => {
+    describe.skip('fail', () => {
 
-        it('error; duplicate text', () => {
-          // todo
-        });
-
+      it('error; duplicate text', () => {
+        // todo
       });
 
     });
